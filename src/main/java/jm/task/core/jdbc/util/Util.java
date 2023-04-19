866421f4042b7b9987a6770a8d0853c7003458c8
sarrust <103333432+sarrust@users.noreply.github.com>
2023-04-19 07:23:30 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String URL = "jdbc:mysql://localhost:3306/my_db";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "rustamov82";
 
@@ -16,7 +16,7 @@ public class Util {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             Statement statement = connection.createStatement();
         } catch (SQLException e) {
-            System.out.println("ОШИБКА! Не удалось соедениться с БД.");;
+            System.out.println("ОШИБКА! Не удалось соедениться с БД.");
         }
         return connection;
     }
